083c00a7e2e8d4a578954aaa8e58dcbeed8fb68c
Kaysha Besi <kayshabesi@gmail.com>
2020-02-16 20:52:18 +0300

    Added a readme.md documment

---
+++ README.md
@@ -0,0 +1,44 @@
+# Akan Names
+#### An application that generates Akan names derived from Ghana, in which people are assigned different names dependingon their date of birth, 15/02/2020
+#### By **Kaysha Besi**
+## Description
+This site takes a user's birthday and calculates the day of the week they were born and then
+depending on their gender outputs their Akan Name. Akan names are derived from Ghanian 
+culture. Frequently in Ghana, children are given their first name as a 'day name' which 
+corresponds to the day in the week they were born. Input the required fields down below to
+generate your Akan name.
+## Setup/Installation Requirements
+* This is a great place
+* to list setup instructions
+* in a simple
+* easy-to-understand
+* format
+{Leave nothing to chance! You want it to be easy for potential users, employers and collaborators to run your app. Do I need to run a server? Do I need to install any dependencies? How should I set up my databases? Is there other code this app depends on?}
+## Known Bugs
+This web application is not always guaranteed to give the exact dates, on some days it'll be exact mostly in years like 2019, but years earliser than that, the outputs will be inaccurate.
+## Technologies Used
+For this i used HTML,CSS and Javascript. I incoporated the use of a ready made stylesheet(bootstrap). I found it much easier to style my landing page this way. In addition to that, i added my own stylesheet when i needed to input my custom styles.
+## Support and contact details
+If you run into any issues or have questions, ideas or concerns. Contact me through email, kayshabesi@gmail.com if you'd like to make any contribution to the code.
+### License
+MIT License
+
+Copyright (c) [2020] [Kaysha Besi]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 function akan(){
+var year= document.getElementById("Year").value;
+var Y = parseInt(year);
 var CC = (Y - 1) / 100 + 1;
-var Y = document.getElementById("Year").value;
-var M = document.getElementById("Month").value;
-var D = document.getElementById("Day").value;
-var dayOfTheWeek  = ( ( (CC/4) -2*CC-1) + ((5*Y/4) ) + ((26*(M+1)/10)) + D )%7;
+var month = document.getElementById("Month").value;
+var M =parseInt(month);
+var day = document.getElementById("Day").value;
+var D = parseInt(day);
+var dayOfTheWeek = parseInt( ( (CC/4) -2*CC-1) + ((5*Y/4) ) + ((26*(M+1)/10)) + D )%7;
 
-var male = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
-var female = ["Akosua","Adowa","Abena","Akua","Yaa","Afua","Ama"];
+var dayOfTheWeek = Math.ceil(dayOfTheWeek);
+
+var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+var femaleNames = ["Akosua","Adowa","Abena","Akua","Yaa","Afua","Ama"];
 
 if (document.getElementById("gender").checked ){
     var gender = "male";
@@ -14,52 +19,63 @@ if (document.getElementById("gender").checked ){
 else{
     var gender = "female";
 }
-}
+
+console.log(M)
+console.log(D)
+console.log(Y)
+
+
 if (M < 1 || M > 12 || M === 2 && D > 29){
     alert("Please enter an actual month.");
 }
-else if (Y <= 1899 || Y >= 2020){
+else if(Y <= 1899 || Y >= 2020){
+
     alert("Please enter an actual year.");
 }
-if (Math.floor(dayOfTheWeek)== 1 && gender=== "male"){
-    document.getElementById("enter").innerHTML ="Your name is" + male[1] + "because you were born on Monday";
+ else if (dayOfTheWeek== 1 && gender=== "male"){
+    document.getElementById("enter").innerHTML ="Your name is" + maleNames[1] + "because you were born on Monday";
 }
-else if (Math.floor(dayOfTheWeek)== 2 && gender=== "male"){
-    document.getElementById("enter").innerHTML ="Your name is" + male[2] + "because you were born on Tuesday";
+  if (dayOfTheWeek == 2 && gender=== "male"){
+    document.getElementById("enter").innerHTML ="Your name is" + maleNames[2] + "because you were born on Tuesday";
 }
-else if (Math.floor(dayOfTheWeek)== 3 && gender=== "male"){
-    document.getElementById("enter").innerHTML ="Your name is" + male[3] + "because you were born on Wednesday";
+else if (dayOfTheWeek== 3 && gender=== "male"){
+    document.getElementById("enter").innerHTML ="Your name is" + maleNames[3] + "because you were born on Wednesday";
 }
-else if (Math.floor(dayOfTheWeek)== 4 && gender=== "male"){
-    document.getElementById("enter").innerHTML ="Your name is" + male[4] + "because you were born on Thursday";
+else if (dayOfTheWeek== 4 && gender=== "male"){
+    document.getElementById("enter").innerHTML ="Your name is" + maleNames[4] + "because you were born on Thursday";
 }
-else if (Math.floor(dayOfTheWeek)== 5 && gender=== "male"){
-    document.getElementById("enter").innerHTML ="Your name is" + male[5] + "because you were born on Friday";
+else if (dayOfTheWeek== 5 && gender=== "male"){
+    document.getElementById("enter").innerHTML ="Your name is" + maleNames[5] + "because you were born on Friday";
 }
-else if (Math.floor(dayOfTheWeek)== 6 && gender=== "male"){
-    document.getElementById("enter").innerHTML ="Your name is" + male[6] + "because you were born on Saturday";
+else if (dayOfTheWeek== 6 && gender=== "male"){
+    document.getElementById("enter").innerHTML ="Your name is" + maleNames[6] + "because you were born on Saturday";
 }
-else if (Math.floor(dayOfTheWeek)== 0 && gender=== "male"){
-    document.getElementById("enter").innerHTML ="Your name is" + male[0] + "because you were born on Sunday";
+else if (dayOfTheWeek== 0 && gender=== "male"){
+    document.getElementById("enter").innerHTML ="Your name is" + maleNames[0] + "because you were born on Sunday";
 }
-else if (Math.floor(dayOfTheWeek)== 1 && gender=== "female"){
-    document.getElementById("enter").innerHTML ="Your name is" + female[1] + "because you were born on Monday";
+else if (dayOfTheWeek== 1 && gender=== "female"){
+    document.getElementById("enter").innerHTML ="Your name is" + femaleNames[1] + "because you were born on Monday";
 }
-else if (Math.floor(dayOfTheWeek)== 2 && gender=== "female"){
-    document.getElementById("enter").innerHTML ="Your name is" + female[2] + "because you were born on Tuesday";
+else if (dayOfTheWeek== 2 && gender=== "female"){
+    document.getElementById("enter").innerHTML ="Your name is" + femaleNames[2] + "because you were born on Tuesday";
 }
-else if (Math.floor(dayOfTheWeek)== 3 && gender=== "female"){
-    document.getElementById("enter").innerHTML ="Your name is" + female[3] + "because you were born on Wednesday";
+else if (dayOfTheWeek== 3 && gender=== "female"){
+    document.getElementById("enter").innerHTML ="Your name is" + femaleNames[3] + "because you were born on Wednesday";
 }
-else if (Math.floor(dayOfTheWeek)== 4 && gender=== "female"){
-    document.getElementById("enter").innerHTML ="Your name is" + female[4] + "because you were born on Thursday";
+else if (dayOfTheWeek== 4 && gender=== "female"){
+    document.getElementById("enter").innerHTML ="Your name is" + femaleNames[4] + "because you were born on Thursday";
 }
-else if (Math.floor(dayOfTheWeek)== 5 && gender=== "female"){
-    document.getElementById("enter").innerHTML ="Your name is" + female[5] + "because you were born on Friday";
+else if (dayOfTheWeek== 5 && gender=== "female"){
+    document.getElementById("enter").innerHTML ="Your name is" + femaleNames[5] + "because you were born on Friday";
 }
-else if (Math.floor(dayOfTheWeek)== 6 && gender=== "female"){
-    document.getElementById("enter").innerHTML ="Your name is" + female[6] + "because you were born on Saturday";
+else if (dayOfTheWeek== 6 && gender=== "female"){
+    document.getElementById("enter").innerHTML ="Your name is" + femaleNames[6] + "because you were born on Saturday";
 }
-else if (Math.floor(dayOfTheWeek)== 0 && gender=== "female"){
-    document.getElementById("enter").innerHTML ="Your name is" + female[0] + "because you were born on Sunday";
+else if (dayOfTheWeek== 0 && gender=== "female"){
+    document.getElementById("enter").innerHTML ="Your name is" + femaleNames[0] + "because you were born on Sunday";
+}
+else{
+    alert("Enter correct information");
+}
+
 }
